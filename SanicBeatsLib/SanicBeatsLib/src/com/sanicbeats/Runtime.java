05d4b6c2a2fb94e68de76e419c4b540435987265
Tyler Hawkins <tylerhawkins@students.suu.edu>
2019-03-22 14:50:10 -0600

    test

--- SanicBeatsLib/SanicBeatsLib/src/com/sanicbeats/Runtime.java
+++ SanicBeatsLib/SanicBeatsLib/src/com/sanicbeats/Runtime.java
@@ -1,12 +1,13 @@
 package com.sanicbeats;
 
 import java.io.IOException;
+import java.lang.Math;
+
 
 public class Runtime {
 	
 	
-	public static void main(String[] args) throws IOException
-	{
+	public static void main(String[] args) throws IOException{
 		try
 		{
 			System.out.println("Beginning Test...");
@@ -21,16 +22,58 @@ public class Runtime {
 		{
 			//Cleanup code if needed
 			
+			int [] a = new int [10];
+			
+			for (int i = 0; i < a.length; i ++) {
+				a[i] = (int)(Math.random()*100);
+				System.out.print(a[i] + "  " );
+			}
+			
+			System.out.println();
+			
+			int [] b = reverise(a);
+			
+			for (int i = 0; i < b.length; i ++) {
+				
+				System.out.print(b[i] + "  " );
+			}
+			System.out.println();
 			System.out.println("Test Complete.\nPress enter to exit.");
 			System.in.read();
 			
 		}
 		
 		System.out.println("The sysyem finished and exicuted as indended");
-		System.out.println("hr treavor");
-		System.out.println("Heuigd");
+
+	}
+
+	public static int[] reverise(int[] a) {
+		int [] b = new int [a.length];
+		for (int i = 0; i < b.length; i ++) {
+			int max = Theromax() / 2;
+			int t = a[i] - max;
+			b[i] = max - t;
+			
+		}
+			
+		return b;
+			
 	}
 	
+	public static int max (int[] a) {
+		int t = 0;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] < t ) {
+				t = a[i];
+			}
+			
+		}
+		
+		return t;
+	}
 	
+	public static int Theromax () {
+		return 100;
+	}
 	
 }

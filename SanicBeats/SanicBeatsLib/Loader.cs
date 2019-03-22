0cc11db599b04d9fcd9de5328acfb0be30ac2746
Josh <joshhess13@gmail.com>
2019-03-22 13:43:46 -0600

    Added sanicbeatslib project to c# solution and included ikvm.

---
+++ SanicBeats/SanicBeatsLib/Loader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System;
+using Exception = System.Exception;
+using java.lang;
+using java.net;
+
+namespace SanicBeatsLib
+{
+    public static class Loader
+    {
+
+        public static void LoadJavaLibs()
+        {
+            URLClassLoader loader = new URLClassLoader(new URL[]{
+                new URL("file:SanicBeatsLib.jar"),
+
+            });
+
+            try
+            {
+                // load the Class
+                Class cl = Class.forName("hello.HelloWorld", true, loader);
+
+                // Create a Object via Java reflection
+                object obj = cl.newInstance();
+                Console.WriteLine(obj);
+                obj = cl.getConstructor(typeof(string)).newInstance("Java");
+                Console.WriteLine(obj);
+
+                //Create a object via C# reflection
+                Type type = ikvm.runtime.Util.getInstanceTypeFromClass(cl);
+                obj = type.GetConstructor(new Type[] { }).Invoke(null);
+                Console.WriteLine(obj);
+                obj = type.GetConstructor(new Type[] { typeof(string) }).Invoke(new object[] { "C#" });
+                Console.WriteLine(obj);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+    }
+}
---
+++ SanicBeats/SanicBeatsLib/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using Exception = System.Exception;
+using java.lang;
+using java.net;
+
+namespace SanicBeatsLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+
+        }
+    }
+}

f26ac89f80cf8bf9aecf0f11b978e7ed65dde40c
Tyler Hawkins <tylerhawkins@students.suu.edu>
2019-04-10 14:33:50 -0600

    todays work

--- SanicBeatsLib/SanicBeatsLib/src/com/sanicbeats/Runtime.java
+++ SanicBeatsLib/SanicBeatsLib/src/com/sanicbeats/Runtime.java
@@ -47,6 +47,7 @@ public class Runtime {
 
 	}
 
+	//makes the lows high and the highs low
 	public static int[] reverise(int[] a) {
 		int [] b = new int [a.length];
 		for (int i = 0; i < b.length; i ++) {
@@ -60,7 +61,7 @@ public class Runtime {
 			
 	}
 	
-	
+	//returns / compresses the array and gets the average of the array for every 2 spots
 	public static int[] average2(int [] a) {
 		int [] b = new int  [(int)Math.ceil(a.length/2)];
 		int j = 0;
@@ -76,7 +77,7 @@ public class Runtime {
 		
 	}
 	
-	
+	//returns / compresses the array and gets the average of the array baised on the number given
 	public static int[] average(int [] a, int n) {
 		double d = Math.ceil((double)(a.length)/n);
 		int [] b = new int  [(int)d];
@@ -98,7 +99,18 @@ public class Runtime {
 		return b;
 		
 	}
+	//sets each spot of the array to a random values baised on the max value in the main array
+	public static int[] random (int [] a) {
+		int max = max(a);
+		int [] b = new int [a.length];
+		for (int i = 0; i < b.length; i++) {
+			b[i] = (int)Math.random()*max;
+			}
+		return b;
+		
+	}
 	
+	// gets the max value in the given array
 	public static int max (int[] a) {
 		int t = 0;
 		for (int i = 0; i < a.length; i++) {
@@ -111,8 +123,43 @@ public class Runtime {
 		return t;
 	}
 	
+	
+	
+	// may be used later
 	public static int Theromax () {
 		return 100;
 	}
 	
+	// returns a random movie quote
+	public static void movie() {
+		int a = (int)Math.random()*8;
+		if (a == 0 ) {
+			System.out.println("'They may take our lives, but they'll never take our freedom!' - Braveheart");
+		}
+		else if (a == 1) {
+			System.out.println("'When you realize you want to spend the rest of your life with somebody, you want the rest of your life to start as soon as possible.' - When Harry Met Sally");
+		}
+		else if (a == 2) {
+			System.out.println("'If you let my daughter go now, that'll be the end of it. I will not look for you, I will not pursue you. But if you don't, I will look for you, I will find you, and I will kill you.' - Taken");
+		}
+		else if (a == 3) {
+			System.out.println("\"Roads? Where we're going we don't need roads.\"\r\n" + 
+					"Back to the Future");
+		}
+		else if (a == 4) {
+			System.out.println("'Hello. My name is Inigo Montoya. You killed my father. Prepare to die.' The Princess Bride");
+		}
+		else if (a == 5) {
+			System.out.println("' Mawage. Mawage is wot bwings us togeder today. Mawage, that bwessed awangment, that dweam wifin a dweam...' - The Princess Bride");
+		}
+		else if (a == 6) {
+			System.out.println("'It is not our abilities that show what we truly are… it is our choices.' – Harry Potter and the Chamber of Secrets");
+		}
+		else if (a == 7) {
+			System.out.println("'My momma always said, 'Life is like a box of chocolates, you never know what you’re gonna get.'' – Forrest Gump");
+		}
+		else if (a == 8) {
+			System.out.println("'How many times do I have to teach you: just because something works doesn’t mean it can’t be improved.' — Black Panther");
+		}
+	}
 }
